3e1f5bdc0b0fcf9816eb9e2d49f98e5f41e351de
Gabriel Spierrer <spierrerg@gmail.com>
2021-04-07 21:39:59 -0300

    segundo commit

---
+++ database/migrations/2021_04_08_002239_create_motos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('motos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('marca');
+            $table->string('modelo');
+            $table->string('color');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('motos');
+    }
+}
